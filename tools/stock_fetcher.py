899ab4a70ba2c5b94ac97055704ba12899e29796
mukund ballav bhatt <mukundballavbhatt@gmail.com>
2026-03-14 05:37:57 +0000

    Add historical stock data support in stock_fetcher

--- tools/stock_fetcher.py
+++ tools/stock_fetcher.py
@@ -1,12 +1,19 @@
 import yfinance as yf
 
-def get_stock_price(symbol):
-    try:
-        stock = yf.Ticker(symbol)
-        info = stock.history(period="1d")
-        price = info["Close"].iloc[-1]
-        return round(price, 2)
 
-    except Exception as e:
-        print("Stock fetch error:", e)
-        return None
+def get_stock_data(symbol):
+    try:
+        stock=yf.Ticker(symbol) #create an object representing a specific stock
+        hist= stock.history(period="1mo") # this result in pandas dataframe
+        price=hist["Close"].iloc[-1] # select last row of close column
+
+        prices=hist["Close"].tolist()
+# convert pandas series to pandas list
+
+        return{
+            "price":round(price,2),
+            "prices":prices
+        }
+    except Exception as e :
+        print("Stock fetch error:",e)
+        return None        
